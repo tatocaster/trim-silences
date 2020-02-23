6a7bdce05350e8913cb81b5ec0fe514f5b98c33a
Merab Tato Kutalia (tatocaster) <kutaliatato@gmail.com>
2020-02-24 00:57:11 +0400

    calculate threshold using track dBFS

--- trim_silences.py
+++ trim_silences.py
@@ -3,12 +3,15 @@ from pydub import AudioSegment
 import time
 import os
 
+
 def get_file_size_in_megabytes(file_path):
     size = os.path.getsize(file_path)
     return size / (1024 * 1024)
 
+
 def get_audio_duration_in_seconds(sound_file):
-	return len(sound_file) / (1000 * 60)
+    return len(sound_file) / (1000 * 60)
+
 
 start_time = time.time()
 
@@ -16,8 +19,11 @@ input_file_path = 'audio/input.wav'
 output_file_path = 'audio/output.wav'
 
 sound_file = AudioSegment.from_wav(input_file_path)
-stripped_audio = AudioSegment.strip_silence(sound_file, silence_len=1100, silence_thresh=-25, padding=200)
+sound_dBFS = sound_file.dBFS
+stripped_audio = AudioSegment.strip_silence(
+    sound_file, silence_len=1100, silence_thresh=sound_dBFS - 10, padding=200)
 stripped_audio.export(output_file_path, format='wav')
+print "input dBFS: {}".format(sound_dBFS)
 
 elapsed_time = time.time() - start_time
 elapsed_time = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
